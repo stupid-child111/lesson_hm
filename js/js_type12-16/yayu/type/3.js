7e392b1315d8c64565a2ab8b5a88d06834963fb2
stupid-child111 <3468619937@qq.com>
2024-12-18 22:38:14 +0800

    修改一点内容

--- js/js_type12-16/yayu/type/3.js
+++ js/js_type12-16/yayu/type/3.js
@@ -6,8 +6,10 @@ console.log(Object.is(+0,-0));
 console.log(2 * "a",2 + "a");
 //null无法typeof
 console.log(typeof(NaN))
-console.log(typeof(NULL))
+console.log(typeof(null))
 console.log(parseInt("123aaa"))
 console.log(NaN === NaN)//不代表确切值
 //不能通过===NaN 判断 所以使用 isNaN
 console.log(isNaN(NaN),isNaN(parseInt("abc")))
+
+console.log(Math.sqrt(-1))
