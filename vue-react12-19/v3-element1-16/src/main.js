fdf221a908522eb3cf08b5ae806ad62d20d704c5
stupid-child111 <3468619937@qq.com>
2025-01-16 17:02:48 +0800

    引入Pinia，安装Stylus，引入全局CSS

--- vue-react12-19/v3-element1-16/src/main.js
+++ vue-react12-19/v3-element1-16/src/main.js
@@ -6,8 +6,10 @@ import {
 } from 'element3'
 import 'element3/lib/theme-chalk/index.css'
 import router from './router'
+import { createPinia } from 'pinia'
 
 createApp(App)
 .use(router)
+.use(createPinia())
 .use(ElButton)  //UI组件库  提升开发效率
 .mount('#app')
