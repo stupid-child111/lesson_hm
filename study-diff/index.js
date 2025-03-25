325f6cf182d0bebabc5dfc4aa13a2229f2a5732d
stupid-child111 <3468619937@qq.com>
2025-03-25 15:56:37 +0800

    感受diff算法的魅力

--- study-diff/index.js
+++ study-diff/index.js
@@ -1,26 +1,42 @@
-//主文件引入  
-import h from './mysnabbdom/h'
-const myVnode1 = h('div',{},'text')
-console.log(myVnode1)
-const myVnode2 = h('div',{},[
-    h('p',{},'我是p标签1'),
-    h('p',{},'我是p标签2'),
-    h('p',{},[
-        h('span',{},'我是span标签1'),
-        h('span',{},'我是span标签2')
-    ]),
+import {
+    init,
+    classModule,
+    propsModule,
+    styleModule,
+    eventListenersModule,
+    h
+  } from "snabbdom";
+
+  //得到盒子和按钮
+const container = document.getElementById('container');
+const button = document.getElementById('btn');
+
+  //2、创建 patch 函数
+  const patch = init([
+    // 通过传入模块初始化 patch 函数
+    classModule, // 开启 classes 功能
+    propsModule, // 支持传入 props
+    styleModule, // 支持内联样式同时支持动画
+    eventListenersModule // 添加事件监听
+  ]);
+  
+
+const vnode1 = h('ul',{},[
+    h('li',{},'A'),
+    h('li',{},'B'),
+    h('li',{},'C')
 ])
-console.log(myVnode2)
-const myVnode3 = h('div',{},h('p',{},'我是p标签'))
-console.log(myVnode3)
-const myVnod4= h("ul", {}, [
-    h("li", {}, "苹果"),
-    h("li", {}, "香蕉"),
-    h("li", {}, [
-      h("div", {}, [
-        h("p", {}, "我是一个p标签"),
-        h("p", {}, "我是一个p标签")
-      ])
-    ])
+
+patch(container,vnode1)
+
+const vnode2 = h('ul',{},[
+    h('li',{key:"A"},'E'),
+    h('li',{key:"B"},'A'),
+    h('li',{key:"C"},'B'),
+    h('li',{key:"D"},'C'),
 ]);
-console.log(myVnod4)
+
+//点击按钮，更新视图  vnode1 -> vnode2
+button.onclick = function(){
+    patch(vnode1,vnode2)
+};
--- study-diff/readme.md
+++ study-diff/readme.md
@@ -8,3 +8,39 @@ h('a', { props: { href:'http://www.example.com'}},'示例');
 <a href="http://www.example.com">示例</a>
 
 - h 函数可以嵌套使用
+
+
+- 实现h函数
+  h('div',{},'text')
+  h('div',{},h())
+  h('div',{},[])
+
+- diff算法
+  1.最小量更新  key很重要
+  2.**只有是同一个虚拟节点，才能进行精细化比较**(先比较  选择器 --> 后 key)
+  3.**只进行同层比较，不进行跨层比较**
+但是2  3 一般不会发生,需求太离谱了
+比如2.
+<ul v-if="mmm">
+<li></li>
+<li></li>
+<li></li>
+</ul>
+<ol v-else>
+<li></li>
+<li></li>
+<li></li>
+</ol>
+<a1v>
+3.
+<section v-if="mmm">
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+</section>
+<p v-if="!mmm"></p>
+<p v-if="!mmm"></p>
+<p v-if="!mmm"></p>
+<p v-if="!mmm"></p>
+</div>
